cce6d1dd9d8a964b18e2f082cec02b1880400ccf
秋人 <syuuto20030912@icloud.com>
2025-08-30 14:40:33 +0900

    ヘッダー作成

--- team-dev/src/app/page.tsx
+++ team-dev/src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            ヘッダー作成用です
+            ヘッダー作成
           </li>
         </ol>
 
